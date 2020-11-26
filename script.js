47c03e07ff5ef66c7f807c6ec9dbd2f406053828
Debora <deboraro.61@gmail.com>
2020-11-26 16:07:35 -0300

    gerado o cactus através do js e adicionado estilos pelo css

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const dino = document.querySelector(".dino");
+const background = document.querySelector(".background");
+let isJumping = false;
+
 
 function handleKeyUp(event){
   if(event.keyCode === 32){
@@ -8,6 +11,7 @@ function handleKeyUp(event){
 
 function jump(){
   let position = 0;
+  isJumping = true;
   //inicio de upInterval
   let upInterval = setInterval(
     function(){
@@ -19,6 +23,7 @@ function jump(){
           function(){
             if(position <= 0 ){
               clearInterval(downInterval);
+              isJumping = false;
             }
             else{
               position -= 20;
@@ -36,5 +41,15 @@ function jump(){
 //fim da upInterval
 }
 
+function createCactus(){
+  let cactus = document.createElement("div");
+  let cactusPosition = 1000;
+
+  cactus.classList.add("cactus");
+  cactus.style.left = cactusPosition + "px";
+  background.appendChild(cactus);
+}
+
+createCactus();
 document.addEventListener("keyup", handleKeyUp);
 
