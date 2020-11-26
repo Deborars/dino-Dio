e1ad9191ba3eac84792664b82294169ecec95ef5
Debora <deboraro.61@gmail.com>
2020-11-26 17:25:46 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function createCactus(){
     }
     else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
       clearInterval(leftInterval);
-      document.body.innerHTML= "<h1 class='game-over'>Fim de jogo</h1>";
+      document.body.innerHTML= "<h1 class='game-over'>Game Over</h1>";
     }
     else{
       cactusPosition -=10;
