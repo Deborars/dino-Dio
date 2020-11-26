7d01e11daf57a5f53098b596e7478c2d2987aa36
Debora <deboraro.61@gmail.com>
2020-11-26 16:42:23 -0300

    adicionada a verificação de colisão entre o dino e o cactus, além da mensagem de game over quando ocorrer

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const dino = document.querySelector(".dino");
 const background = document.querySelector(".background");
 let isJumping = false;
-
+let position = 0;
 
 function handleKeyUp(event){
   if(event.keyCode === 32){
@@ -10,7 +10,7 @@ function handleKeyUp(event){
 }
 
 function jump(){
-  let position = 0;
+  
   isJumping = true;
   //inicio de upInterval
   let upInterval = setInterval(
@@ -23,7 +23,7 @@ function jump(){
           function(){
             if(position <= 0 ){
               clearInterval(downInterval);
-              isJumping = false;
+              //isJumping = false;
             }
             else{
               position -= 20;
@@ -44,10 +44,28 @@ function jump(){
 function createCactus(){
   let cactus = document.createElement("div");
   let cactusPosition = 1000;
+  let randomTime = Math.random()*6000;
 
   cactus.classList.add("cactus");
-  cactus.style.left = cactusPosition + "px";
+  cactus.style.left = 1000 + "px";
   background.appendChild(cactus);
+
+  let leftInterval = setInterval(function(){
+    if(cactusPosition <= -60){//foi colocado 60 devido ao tamanho do cactus;
+      clearInterval(leftInterval);
+      background.removeChild(cactus);
+    }
+    else if(cactusPosition > 0 && cactusPosition < 60 && position < 60){
+      clearInterval(leftInterval);
+      document.body.innerHTML= "<h1 class='game-over'>Fim de jogo</h1>";
+    }
+    else{
+      cactusPosition -=10;
+      cactus.style.left = cactusPosition +"px";
+    }
+  },20);
+  
+  setTimeout(createCactus, randomTime);
 }
 
 createCactus();
